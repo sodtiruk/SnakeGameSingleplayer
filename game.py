4f8036b3eb5d79bb606bef70ebab4ff07d0091ba
sodtiruk22 <sodtiruk22@gmail.com>
2023-12-30 03:46:52 +0700

    create class snake and field

--- game.py
+++ game.py
@@ -1,9 +1,28 @@
 from tkinter import *
 
 
+class Field(Canvas):
+
+    def __init__(self, w, h):
+        super().__init__(width=w, height=h, background='black', highlightthickness=0)
 
 
 
+class Snake:
+    def __init__(self):
+        pass        
+    
+
+if __name__ == '__main__':
+    game = Tk()
+
+    # Create field game
+    fieldgame = Field(600, 620)
+    fieldgame.pack()
+
+    # Snake
+
+    game.mainloop()
 
 
 

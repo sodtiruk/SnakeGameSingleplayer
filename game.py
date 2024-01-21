b719445af34e9db924fbb80a4e98a26a257201d2
sodtiruk <sodtiruk22@gmail.com>
2024-01-22 06:00:49 +0700

    change function createObjectSnake create function moveObjectSnake onKeyPress runGame and Create fundction in class Snake getImageBody

--- game.py
+++ game.py
@@ -3,27 +3,49 @@ from PIL import ImageTk
 
 class Game(Canvas):
 
-    def __init__(self, w, h):
-        super().__init__(width=w, height=h, background='black', highlightthickness=0)
+    def __init__(self, snake, food):
+        super().__init__(width=600, height=620, background='black', highlightthickness=0)
+        self.snake = snake
+        self.food = food
 
-    def create_object(self, objects, position, tagname):
-        #create object
+        self.bind_all('<Key>', self.onKeyPress)
+
+    def createObjectSnake(self):
+        #create object and forloop for draw box body snake
+        position = self.snake.getPostion()
+        imageBody = self.snake.getImageBody()
         for x_pos, y_pos in position:
-            self.create_image(x_pos, y_pos, image=objects, tags=tagname)
+            self.create_image(x_pos, y_pos, image=imageBody, tags='snake')
+            print(x_pos, y_pos) 
+
+    def moveObjectSnake(self):
+        position = self.snake.getPostion()
+
+    def onKeyPress(self, e):
+        direction = e.keysym
+        print(direction)
+
+    def runGame(self):
+
+        self.after(500, self.runGame)
 
 
 class Snake:
     def __init__(self):
         # position        head--------------------tail
         self.position = [(100,100), (80, 100), (60, 100)]
-        self.snake = ImageTk.PhotoImage(file='assets/body.png')
-    
+        self.imageBody = ImageTk.PhotoImage(file='assets/body.png')
+                
+
     def getPostion(self):
         return self.position 
 
-    def setPostion(self, x, y):
+    def setPostion(self, x, y, direction):
         pass
-        
+
+    def getImageBody(self):
+        return self.imageBody
+
 class Food():
     pass
     
@@ -31,11 +53,13 @@ class Food():
 if __name__ == '__main__':
     app = Tk()
 
+    #create class snake and class food
+    snake = Snake()
+    food = Food()
     # Create field game
-    game = Game(600, 620)
-
-    # Snake
-
+    game = Game(snake, food)
+    game.createObjectSnake()
+    game.runGame()
 
 
 

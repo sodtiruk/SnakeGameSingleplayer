3f30906e677fa1bb3cf4f1eefa219a71b46dfe44
sodtiruk <sodtiruk22@gmail.com>
2024-01-23 02:52:31 +0700

    create class food and draw food in game

--- game.py
+++ game.py
@@ -11,16 +11,16 @@ class Game(Canvas):
         self.direction = 'Right'
         self.bind_all('<Key>', self.onKeyPress)
 
+    #Snake solution ===========================>
     def createObjectSnake(self):
         #create object and forloop for draw box body snake
-        position = self.snake.getPostion()
+        position = self.snake.getPosition()
         imageBody = self.snake.getImageBody()
         for x_pos, y_pos in position:
             self.create_image(x_pos, y_pos, image=imageBody, tags='snake')
-            print(x_pos, y_pos) 
 
     def moveObjectSnake(self):
-        position = self.snake.getPostion()        
+        position = self.snake.getPosition()        
         headx, heady = position[0]
 
         if self.direction == 'Right':
@@ -31,46 +31,57 @@ class Game(Canvas):
             newPosition = [(headx, heady - 20)] + position[:-1]
         if self.direction == 'Down':
             newPosition = [(headx, heady + 20)] + position[:-1]
-        
-        self.snake.setPostion(newPosition) 
+        self.snake.setPosition(newPosition) 
 
         tagsIdSnakes = self.find_withtag('snake')
-        print(tagsIdSnakes)
-
         #Draw body images snake move 
         for tagsIdSnakes, position in zip(tagsIdSnakes, newPosition):
             self.coords(tagsIdSnakes, position)
 
+    def runGame(self):
+        self.moveObjectSnake()
+        self.after(70, self.runGame)
+    
+    #Food solution =====================>
+    def createObjectFood(self):
+        position = self.food.getPosition()
+        imageFood = self.food.getImageFood()
+        #Draw food
+        self.create_image(position, image=imageFood, tags="Food")
+
     def onKeyPress(self, e):
         direction = e.keysym
         self.direction = direction
-        print(self.direction)
-
-    def runGame(self):
-        self.moveObjectSnake()
-        self.after(500, self.runGame)
-
 
 class Snake:
     def __init__(self):
         # position        head--------------------tail
         self.position = [(100,100), (80, 100), (60, 100)]
-        self.imageBody = ImageTk.PhotoImage(file='assets/body.png')
+        self.imageBody = ImageTk.PhotoImage(file="assets/body.png")
 
-    def getPostion(self):
+    def getPosition(self):
         return self.position 
 
-    def setPostion(self, newPosition):
+    def setPosition(self, newPosition):
         self.position = newPosition
-        print(self.position)        
 
     def getImageBody(self):
         return self.imageBody
 
 class Food():
-    pass
+    def __init__(self):
+        self.position = (20, 20) 
+        self.imageFood = ImageTk.PhotoImage(file="assets/food.png")   
     
-   
+    def getPosition(self):
+        return self.position
+    
+    def setPosition(self, newPosition):
+        self.position = newPosition
+
+    def getImageFood(self):
+        return self.imageFood
+
 if __name__ == '__main__':
     app = Tk()
     app.title("Snake Game")
@@ -80,7 +91,10 @@ if __name__ == '__main__':
     # Create field game
     game = Game(snake, food)
     game.createObjectSnake()
+    game.createObjectFood()
     game.runGame()
+
+    
 
     game.pack()
     app.mainloop()
